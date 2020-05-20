e0f8e82616f04ac52294d832bfdddf4e7c52bf78
Alexei Matveev <alexei.matveev@gmail.com>
2020-05-20 19:26:58 +0200

    AM: ring 1.8.1

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [bidi "2.1.6"]
-                 [ring/ring-core "1.7.1"]
-                 [ring/ring-json "0.5.0"]
-                 [ring/ring-jetty-adapter "1.7.1"]]
+                 [ring/ring-core "1.8.1"]
+                 [ring/ring-jetty-adapter "1.8.1"] ; same as ring-core
+                 [ring/ring-json "0.5.0"]]
   :repl-options {:init-ns datasource-clj.core}
   :main datasource-clj.core
   :aot [datasource-clj.core])
