6654707a4201ab3d047f411f0f0eb258bd4887ec
Alexei Matveev <alexei.matveev@gmail.com>
2020-05-20 19:18:16 +0200

    AM: url and description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject datasource-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Grafana JSON data source backend"
+  :url "https://github.com/alexei-matveev/datasource-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
